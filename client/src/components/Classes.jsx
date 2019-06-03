26bfe8028c6088b03cb714dada93f6d7161022e3
Omar Rashid <omar.rashid2@gmail.com>
2019-06-03 11:50:34 -0500

    Add documentation for class component

--- client/src/components/Classes.jsx
+++ client/src/components/Classes.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ClassListItem from './ClassListItem.jsx';
 
+/**
+ * Classes is a stateless react component that displays a class list item for each class
+ * @param {*} props - the passed down props (classList is an array prop)
+ */
+
 const Classes = props => (
   <div>
     <h4 display="inline-block">Your Current Classes</h4>
