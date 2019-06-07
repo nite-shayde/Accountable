ba37fe8540c2c8bbc46918001e965c523bd1af8d
Jesse Lessinger <jesse.lessinger@gmail.com>
2019-06-07 14:43:25 -0500

    Add style to teacher home

--- client/src/components/TeacherHome.jsx
+++ client/src/components/TeacherHome.jsx
@@ -203,96 +203,105 @@ class TeacherHome extends React.Component {
     const {
       currentTeacherName, renderInput, currentTeacherId, currentTeacherClasses, students, allClasses,
     } = this.state;
+
     return (
-      <div>
-        <div className="jumbotron jumbotron-fluid">
-          <div className="header">
-            <h1 className="title">Accountable</h1>
-            <p id="quote">Experience teaches only the teachable.</p>
-            <p id="quoteauthor">-Aldous Huxley</p>
-            <button type="submit" text-align="right" className="btn btn-sm" id="logoutButt" onClick={logout}>Log Out</button>
+      <div className="">
+        <div className="sidebar d-flex flex-column p-3 flex-grow-0">
+
+          <div className="d-flex justify-content-end"><button type="submit" className="btn btn-sm btn-dark" id="" onClick={logout}>Log Out</button></div>
+          <div className="">
+            <h3>Welcome back</h3>
+            <h4>{currentTeacherName}</h4>
+          </div>
+          <div>
+            <button type="submit" className="btn btn-success mb-3" onClick={this.renderClassInput}>Add Class</button>
+            {renderInput
+              ? (
+                <div>
+                  <input placeholder="new class name" onChange={this.changeInputState} />
+                  <button type="submit" className="btn btn-primary btn-sm mb-4" onClick={this.submitClassHandler}>Submit</button>
+                </div>
+              )
+              : null }
+          </div>
+          <div>
+            <button type="submit" className="btn btn-success" onClick={this.toggleMassTextModal}>mass text</button>
           </div>
         </div>
-        <div className="greeting">
-          <h4>
-            Welcome Back,
-            {currentTeacherName}
-          </h4>
-        </div>
-        <br />
-        <div className="classes">
-          <button type="submit" className="btn btn-dark btn-sm" onClick={this.renderClassInput}>Add Class</button>
-          <button type="submit" className="btn btn-dark btn-sm" onClick={this.toggleMassTextModal}>mass text</button>
-          {renderInput
-            ? (
-              <div>
-                <input placeholder="new class name" onChange={this.changeInputState} />
-                <button type="submit" className="btn btn-dark btn-sm" onClick={this.submitClassHandler}>Submit</button>
-              </div>
-            )
-            : null }
-          <Classes
-            teacherID={currentTeacherId}
-            teacherName={currentTeacherName}
-            classList={currentTeacherClasses}
-          />
-        </div>
-        <div className="select box">
-          {/* <select onChange={this.studentInfo}> */}
-          {/* <option value="title">Select Student</option> */}
-          { students.map(student => (
-            <StudentModal currentStudent={student} name={student.name} teacherName={currentTeacherName} />
+
+        <div className="main-panel">
+          <div className="shadow rounded mt-3 bg-light py-3 px-5 mb-3">
+            <div className="header d-flex flex-column">
+              <h1 className="text-primary">Accountable</h1>
+              <div id="quote" className="font-italic d-flex justify-content-center">If you want to make an apple pie from scratch, you must first create the universe.</div>
+              <h6 className="d-flex justify-content-center">- Carl Sagan</h6>
+            </div>
+          </div>
+
+          <div className="classes">
+            <Classes
+              teacherID={currentTeacherId}
+              teacherName={currentTeacherName}
+              classList={currentTeacherClasses}
+            />
+          </div>
+          <div className="select box">
+            {/* <select onChange={this.studentInfo}> */}
+            {/* <option value="title">Select Student</option> */}
+            { students.map(student => (
+              <StudentModal currentStudent={student} name={student.name} teacherName={currentTeacherName} />
             // <option value="student">{student.name}</option>
-          )) }
-          {/* </select> */}
-        </div>
-        {/** MASS TEXT MODAL */}
-        <Modal
-          show={showMassTextModal}
-          onHide={this.toggleMassTextModal}
-          dialogClassName="modal-90w"
-        >
-          <ModalHeader>
-            <ModalTitle id="title" />
-            {/* <Button className="btn btn-sm btn-dark" onClick={this.showHistory} id="history">View Comment History</Button>
+            )) }
+            {/* </select> */}
+          </div>
+          {/** MASS TEXT MODAL */}
+          <Modal
+            show={showMassTextModal}
+            onHide={this.toggleMassTextModal}
+            dialogClassName="modal-90w"
+          >
+            <ModalHeader>
+              <ModalTitle id="title" />
+              {/* <Button className="btn btn-sm btn-dark" onClick={this.showHistory} id="history">View Comment History</Button>
 
             <Button className="btn btn-sm btn-dark" onClick={this.newComment} id="newComment">Leave a Comment</Button> */}
-            <h3>Mass Text</h3>
-          </ModalHeader>
-          <ModalBody>
-            <form action="/texts" method="post" onSubmit={this.sendMassText}>
-              {/* <button type="button" value="false" className="btn btn-info btn-sm" onClick={this.selectAll}>select all</button> */}
-              <input type="checkbox" onClick={this.selectAll} />
-              <h5 className="d-inline text-secondary ml-1">select/deselect all</h5>
-              <fieldset disabled={formDisabled} className="mt-2">
-                { allClasses.map(clss => (
-                  <div>
-                    <fieldset>
-                      <input type="checkbox" onClick={this.selectAll} />
-                      <h4 className="d-inline ml-1">
-                        { clss.name }
-                      </h4>
-                      { clss.students.map(student => (
-                        <div className="ml-3">
-                          <input type="checkbox" name={student.id} value={student.phone} onClick={this.toggleNumber} />
-                          <span className="ml-1">{student.name}</span>
-                        </div>
-                      )) }
-                    </fieldset>
-                  </div>
-                )) }
-                <textarea className="mt-2" name="text-message" value={massTextMessage} onChange={this.changeMassTextMessage} />
-                <div><input type="submit" value="Submit" /></div>
-              </fieldset>
-            </form>
-            <div className="text-success">
-              <h5>{confirmMessage}</h5>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={this.handleHide} className="btn btn-sm btn-dark">Close</Button>
-          </ModalFooter>
-        </Modal>
+              <h3>Mass Text</h3>
+            </ModalHeader>
+            <ModalBody>
+              <form action="/texts" method="post" onSubmit={this.sendMassText}>
+                {/* <button type="button" value="false" className="btn btn-info btn-sm" onClick={this.selectAll}>select all</button> */}
+                <input type="checkbox" onClick={this.selectAll} />
+                <h5 className="d-inline text-secondary ml-1">select/deselect all</h5>
+                <fieldset disabled={formDisabled} className="mt-2">
+                  { allClasses.map(clss => (
+                    <div>
+                      <fieldset>
+                        <input type="checkbox" onClick={this.selectAll} />
+                        <h4 className="d-inline ml-1">
+                          { clss.name }
+                        </h4>
+                        { clss.students.map(student => (
+                          <div className="ml-3">
+                            <input type="checkbox" name={student.id} value={student.phone} onClick={this.toggleNumber} />
+                            <span className="ml-1">{student.name}</span>
+                          </div>
+                        )) }
+                      </fieldset>
+                    </div>
+                  )) }
+                  <textarea className="mt-2" name="text-message" value={massTextMessage} onChange={this.changeMassTextMessage} />
+                  <div><input type="submit" value="Submit" className="btn btn-primary" /></div>
+                </fieldset>
+              </form>
+              <div className="text-success">
+                <h5>{confirmMessage}</h5>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={() => this.setState({ showMassTextModal: false })} className="btn btn-sm btn-dark">Close</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
       </div>
     );
   }
